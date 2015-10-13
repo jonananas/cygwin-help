5edc8581fa6c9fdc90b85f9ccdf8db3773f670dc
Jonas Andersson <jonas.a.andersson@umu.se>
2015-10-13 14:44:25 +0100

    added unhijack

--- find_actual_hijacks.sh
+++ find_actual_hijacks.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo Creating diff-list towards master 1>&2
 git diff --name-only master | awk '{print "./"$1}'  | sort  > /tmp/actual_changes.lst
+echo Creating list of writable .java and pom.xml 1>&2
 find_hijacks.sh | sort > /tmp/potential_hijacks.lst
+echo Listing actual hijacks, ie writables that has not changed: 1>&2
 diff /tmp/actual_changes.lst /tmp/potential_hijacks.lst |grep ">"|awk '{print $2}'
