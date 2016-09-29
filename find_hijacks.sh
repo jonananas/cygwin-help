efe6c1b9308d7cf240512a5bffb44abe6d49270e
Jonas Andersson <jonas.a.andersson@umu.se>
2016-09-29 16:33:03 +0200

    removed clearcase helpers added links (node, npm etc), some changes

--- find_hijacks.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-find . \( -name "*.java" -o -name "pom.xml" \) -perm /u+w|egrep -v "\/(Servers|target|generated)\/"
--- jmeter.sh
+++ jmeter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 calledfrom=`pwd`
 cd  /cygdrive/c/MyPrograms/apache-jmeter-2.13/bin/
-./jmeter -t `cygpath -w ${calledfrom}/$1`
+./jmeter -t `cygpath -w ${calledfrom}/$1` $2 $3 $4
