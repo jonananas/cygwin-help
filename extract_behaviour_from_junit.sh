aa9577bd62e5bcf7032b97923f0d0227ba0af1ea
Jonas Andersson <jonas.a.andersson@umu.se>
2018-02-13 15:05:28 +0100

    added script

--- extract_behaviour_from_junit.sh
+++ extract_behaviour_from_junit.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 file=$1
-basename -s Test.java $file
-(LC_CTYPE=sv_SE.ISO-8859-1;grep -a DisplayName $file|awk '{FS="\""}{print " - "$2}'|grep -v DisplayName)
+basename -s Test.java $file | grep -v .java
+(LC_CTYPE=sv_SE.ISO-8859-1; grep -a DisplayName $file | awk '{FS="\""}{print " - "$2}' | grep -v DisplayName)
