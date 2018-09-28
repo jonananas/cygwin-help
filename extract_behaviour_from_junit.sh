e347c9bbe1be7445783c96d4d1216897c67d1e0f
Jonas Andersson <jonas.a.andersson@umu.se>
2018-09-28 13:29:50 +0200

    updated extract behaviour from junit

--- extract_behaviour_from_junit.sh
+++ extract_behaviour_from_junit.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 file=$1
 basename -s Test.java $file | grep -v .java
-(LC_CTYPE=sv_SE.ISO-8859-1; grep -a DisplayName $file | awk '{FS="\""}{print " - "$2}' | grep -v DisplayName)
+(grep --text DisplayName $file | awk '{FS="\""}{print " - "$2}' | grep -v --text DisplayName | iconv -f ISO-8859-1 -t UTF-8)
+#(LC_CTYPE=sv_SE.ISO-8859-1; grep -a DisplayName $file | awk '{FS="\""}{print " - "$2}' | grep -v DisplayName | iconv -f ISO-8859-1 -t UTF-8)
